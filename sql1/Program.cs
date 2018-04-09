295fd022a22891bfb60dc9e12e04233b5aff0499
JensMalm <jensmalm873@gmail.com>
2018-04-09 09:39:31 +0200

    Hej

--- sql1/Program.cs
+++ sql1/Program.cs
@@ -1,11 +1,27 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlTypes;
 
-namespace sql1
+namespace sql CRM
 {
     class Program
     {
+
+        public void test(string title)
+        {
+            var sql = @"SELECT [";
+
+
+            using (SqlConnection connection = new SqlConnection(conString))
+                using(Sqlcommand command=new SqlCommannd(sql,connection))
+
+
+                Command.Parameters.Add(new SqlParameter("apa", title));
+
+        }
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello World!");
         }
     }
